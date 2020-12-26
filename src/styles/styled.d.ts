c2f1f5cd11351527132945bbad4a6528dee769fa
minsour <parkminsoo0128@gmail.com>
2020-12-27 04:08:49 +0900

    Add media to theme for responsive web

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -3,6 +3,12 @@ import 'styled-components';
 
 import { THEME } from '../hooks/useTheme';
 
+export interface Media {
+  mobile: (...args: BackQuoteArgs) => CSSProp | undefined;
+  tablet: (...args: BackQuoteArgs) => CSSProp | undefined;
+  desktop: (...args: BackQuoteArgs) => CSSProp | undefined;
+}
+
 declare module 'styled-components' {
   export interface DefaultTheme {
     backgroundColor: {
@@ -14,5 +20,6 @@ declare module 'styled-components' {
       secondary: string;
     };
     borderColor: string;
+    media: Media;
   }
 }
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,5 +1,58 @@
-// src/styles/theme.ts
 import { DefaultTheme } from 'styled-components';
+import baseStyled, {
+  css,
+  CSSProp,
+  ThemedStyledInterface,
+} from 'styled-components';
+
+import { Media } from './styled';
+
+const sizes: { [key: string]: number } = {
+  mobile: 320,
+  tablet: 768,
+  desktop: 1024,
+};
+
+type BackQuoteArgs = string[];
+
+const media: Media = {
+  mobile: (...args: BackQuoteArgs) => undefined,
+  tablet: (...args: BackQuoteArgs) => undefined,
+  desktop: (...args: BackQuoteArgs) => undefined,
+};
+
+Object.keys(sizes).reduce((acc: Media, label: string) => {
+  switch (label) {
+    case 'desktop':
+      acc.desktop = (...args: BackQuoteArgs) =>
+        css`
+          @media only screen and (min-width: ${sizes.desktop}px) {
+            ${args}
+          }
+        `;
+      break;
+    case 'tablet':
+      acc.tablet = (...args: BackQuoteArgs) =>
+        css`
+          @media only screen and (max-width: ${sizes.desktop}px) and (min-width: ${sizes.tablet}px) {
+            ${args}
+          }
+        `;
+      break;
+    case 'mobile':
+      acc.mobile = (...args: BackQuoteArgs) =>
+        css`
+          @media only screen and (max-width: ${sizes.tablet}px) {
+            ${args}
+          }
+        `;
+      break;
+    default:
+      break;
+  }
+
+  return acc;
+}, media);
 
 const darkTheme: DefaultTheme = {
   backgroundColor: {
@@ -11,6 +64,7 @@ const darkTheme: DefaultTheme = {
     secondary: '#73747b',
   },
   borderColor: '#4a4b50',
+  media,
 };
 
 const lightTheme: DefaultTheme = {
@@ -23,6 +77,7 @@ const lightTheme: DefaultTheme = {
     secondary: '#999',
   },
   borderColor: '#dcdcdc',
+  media,
 };
 
 const themes = {
