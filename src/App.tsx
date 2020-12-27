66e88428381d073c40ed7f7d7c61e66ed190c57f
minsour <parkminsoo0128@gmail.com>
2020-12-27 19:37:45 +0900

    Add About, DearWorld, NotFound Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 import React, { FC } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import Hello from './components/Hello';
 import ThemeButton from './components/ThemeButton';
 import { ThemeProvider } from './hooks/useTheme';
+import { About, CheeringMap, DearWorld, NotFound } from './pages';
 import { GlobalStyle } from './styles/global-styles';
 
 const App: FC = () => {
   return (
-    <ThemeProvider>
-      <GlobalStyle />
-      <Hello name="React" />
-      <ThemeButton />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider>
+        <GlobalStyle />
+        <Switch>
+          <Route exact path="/" component={DearWorld} />
+          <Route exact path="/cheering-map" component={CheeringMap} />
+          <Route exact path="/about" component={About} />
+          <Route component={NotFound} />
+        </Switch>
+        <ThemeButton />
+      </ThemeProvider>
+    </BrowserRouter>
   );
 };
 
---
+++ src/pages/About.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+import { Layout } from '../components';
+
+const About: FC = () => (
+  <Layout>
+    <div>Dear World, We are OFU Crews!</div>
+  </Layout>
+);
+
+export default About;
