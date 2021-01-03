0d405ac9b4658435834cd1a37af1fd4a93461d56
minsour <parkminsoo0128@gmail.com>
2021-01-04 04:49:51 +0900

    Add countries state

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 
 import ThemeButton from './components/ThemeButton';
 import { ThemeProvider } from './hooks/useTheme';
@@ -8,18 +9,20 @@ import { GlobalStyle } from './styles/global-styles';
 
 const App: FC = () => {
   return (
-    <BrowserRouter>
-      <ThemeProvider>
-        <GlobalStyle />
-        <Switch>
-          <Route exact path="/" component={DearWorld} />
-          <Route exact path="/cheering-map" component={CheeringMap} />
-          <Route exact path="/about" component={About} />
-          <Route component={NotFound} />
-        </Switch>
-        <ThemeButton />
-      </ThemeProvider>
-    </BrowserRouter>
+    <RecoilRoot>
+      <BrowserRouter>
+        <ThemeProvider>
+          <GlobalStyle />
+          <Switch>
+            <Route exact path="/" component={DearWorld} />
+            <Route exact path="/cheering-map" component={CheeringMap} />
+            <Route exact path="/about" component={About} />
+            <Route component={NotFound} />
+          </Switch>
+          <ThemeButton />
+        </ThemeProvider>
+      </BrowserRouter>
+    </RecoilRoot>
   );
 };
 
